76be660441645e957398d34aef579c3ea049d927
Michaël Zasso <targos@protonmail.com>
2026-04-21 15:51:21 +0200

    test: skip wasm allocation tests in workers
    Refs: https://github.com/nodejs/node/issues/62870
    PR-URL: https://github.com/nodejs/node/pull/61898
    Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
    Reviewed-By: Filip Skokan <panva.ip@gmail.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
    (cherry picked from commit fcff4581450036484654bb6c03ef5c141cd2f36b)

--- test/wasm-allocation/test-wasm-allocation.js
+++ test/wasm-allocation/test-wasm-allocation.js
@@ -3,5 +3,24 @@
 // allows WASM to at least run with inline bound checks.
 'use strict';
 
-require('../common');
-new WebAssembly.Memory({ initial: 10, maximum: 100 });
+const common = require('../common');
+const assert = require('assert');
+const { isMainThread } = require('worker_threads');
+
+// The first allocation should succeed.
+const first = new WebAssembly.Memory({ initial: 10, maximum: 100 });
+assert(first);
+
+if (!isMainThread) {
+  // https://github.com/nodejs/node/issues/62870
+  common.skip('Workers terminate instead of throwing');
+}
+
+// Subsequent allocations should eventually fail due to running out of
+// virtual address space.
+assert.throws(() => {
+  const instances = [first];
+  for (let i = 1; i < 30; i++) {
+    instances.push(new WebAssembly.Memory({ initial: 10, maximum: 100 }));
+  }
+}, /WebAssembly\.Memory/);
